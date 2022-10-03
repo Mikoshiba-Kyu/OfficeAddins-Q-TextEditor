069c6cb57eeb9e62e5a6c66fc6ce69357d0185a9
Mikoshiba-Kyu <mikoshibakyu@gmail.com>
2022-10-03 21:41:03 +0900

    [add] README.mdを追加

---
+++ README.md
@@ -0,0 +1,8 @@
+# とりあえず
+
+1. `clone` する
+1. VSCodeを管理者で起動する
+1. `npm i` する
+1. `npm start` する
+
+もし `Allow localhost loopback for Microsoft Edge WebView?` がでたら `y` する
--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -11,7 +11,7 @@ initializeIcons()
 
 let isOfficeInitialized = false
 
-const title = "CellPad"
+const title = "PowerQueryEditor-Neo"
 
 const render = (Component) => {
   ReactDOM.render(
